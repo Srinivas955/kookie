12f3cb664202f4f7c17016744ba955f685663f62
Srinivas <kookatlasrinivasyadav@gmail.com>
2022-03-17 19:22:09 +0530

    updated cookies practice

--- src/main/java/in/srinivas/kookie.java
+++ src/main/java/in/srinivas/kookie.java
@@ -2,20 +2,27 @@ package in.srinivas;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 public class kookie extends HttpServlet
 {
-		public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+		public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
 		{
 			int i = Integer.parseInt(req.getParameter("num1"));
 			int j = Integer.parseInt(req.getParameter("num2"));
 			int k = i + j;
 			
-			PrintWriter out = res.getWriter();
+			Cookie cookie = new Cookie("k", k + "");
+			res.addCookie(cookie);
 			
-			out.println("Result is " + k);
+			res.sendRedirect("sq");
+			/*
+			 * PrintWriter out = res.getWriter();
+			 * 
+			 * out.println("Result is " + k);
+			 */
 			
 			
 		}
---
+++ src/main/java/in/srinivas/kservlet.java
@@ -0,0 +1,28 @@
+package in.srinivas;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class kservlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
+		
+		for(Cookie c : cookies)
+			if(c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+				
+		k = k * k;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+		/* System.out.println("sq is called"); */
+	}
+}
